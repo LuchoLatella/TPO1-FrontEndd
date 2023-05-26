c37bb90f88c27c89440eca3f17b0b876683ceb50
LuchoLatella <lucho_latella@hotmail.com>
2023-05-25 21:24:44 -0300

    agregdo js en formulario de contacto

---
+++ js/form_contacto.js
@@ -0,0 +1,21 @@
+<form id="myForm" action="show_data.html" target="_blank" onsubmit="return validarFormulario()">
+
+<script>
+function validarFormulario() {
+  // Obtener referencias a los campos del formulario
+  var nombre = document.forms["myForm"]["firstname"].value;
+  var direccion = document.forms["myForm"]["address"].value;
+  var correo = document.forms["myForm"]["mail"].value;
+
+  // Validar los campos requeridos
+  if (nombre == "" || direccion == "" || correo == "") {
+    alert("Por favor, complete todos los campos requeridos.");
+    return false;
+  }
+
+  // Realizar cualquier otra validación adicional aquí
+
+  // Si la validación se pasa, devolver true para enviar el formulario
+  return true;
+}
+</script>
